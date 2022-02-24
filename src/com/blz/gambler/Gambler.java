4a49661db2fd621c2ae6a07e5deff4a4ffe5dd15
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-24 15:59:09 +0530

    Added: Gambler's Stake and Bet value

--- src/com/blz/gambler/Gambler.java
+++ src/com/blz/gambler/Gambler.java
@@ -1,8 +1,11 @@
 package com.blz.gambler;
 
 public class Gambler {
+    public static final int STAKE=100; // Gambler with the stake
+    public static final int BET=1; // Bet value per game
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Gambler Game");
+        System.out.println("Gambler with stake of "+STAKE+" dollars everyday and to bet "+BET+" dollar per game");
     }
 }

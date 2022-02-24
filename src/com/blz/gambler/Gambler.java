036a09d11bc80d60bdc7ee7c90f41875213a9509
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-24 16:29:19 +0530

    Added:total stake after the bet functionality

--- src/com/blz/gambler/Gambler.java
+++ src/com/blz/gambler/Gambler.java
@@ -5,7 +5,17 @@ public class Gambler {
     public static final int BET=1; // Bet value per game
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Gambler Game");
-        System.out.println("Gambler with stake of "+STAKE+" dollars everyday and to bet "+BET+" dollar per game");
+        int totalStake=STAKE;
+
+           System.out.println("Welcome to the Gambler Game");
+        //initialising game value by random number
+        int game=(int) (Math.random() * 10) % 2;
+        if(game==1){
+            totalStake=totalStake+BET; //Stake will increase as gambler wins the bet
+            System.out.println("Gambler won the bet and now having "+totalStake);
+        }else{
+            totalStake=totalStake-BET; //Stake will decrease as gambler lost the bet
+            System.out.println("Gambler lost the bet and now having "+totalStake);
+        }
     }
 }
